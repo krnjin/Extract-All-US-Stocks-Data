8b23b20c3a053af3bca98c5cf478e5d71ef6a560
Jin Kwon <jinkwon@Jins-MacBook-Air.local>
2022-01-30 20:48:11 -0800

    update

--- stack/stack/spiders/stack_spider.py
+++ stack/stack/spiders/stack_spider.py
@@ -1,4 +1,6 @@
 from scrapy import Spider
+from scrapy.selector import Selector
+from stack.items import StackItem
 
 class StackSpider(Spider):
     name = "companiesmarketcap"
@@ -8,4 +10,9 @@ class StackSpider(Spider):
     ]
 
     def parse(self, response):
-        todays_price = Selector(response).xpath('//td[@class="rh-sm"]/span')
+        todays_price = Selector(response).xpath('//table[@class="table marketcap-table dataTable"]/tbody/tr')
+
+        for price in todays_price:
+            item = StackItem()
+            item['price'] = price.xpath('td[@class="rh-sm"]/span/text()').extract()[0]
+            yield item
